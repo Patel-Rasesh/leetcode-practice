eccf5df44d3389c237ab8d13542da35aff7c760d
Rasesh Patel <91232193+Patel-Rasesh@users.noreply.github.com>
2022-04-24 16:37:24 -0500

    Time: 47 ms (67.48%), Space: 13.6 MB (29.13%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -1,34 +1,34 @@
 class Solution:
     def totalNQueens(self, n):
-        def backtrack(row, diagonals, anti_diagonals, cols):
-            # Base case - N queens have been placed
+        def backtrack(row, diagonals, antiDiagonals, colSet):
+                
+            # 1. If current row is greater than n, solution can be incremented by 1
             if row == n:
                 return 1
-
-            solutions = 0
+           
+            # -1. Check if the position is safe or not
+            def safePosition():
+                if col in colSet or localAntiDiagonal in antiDiagonals or localDiagonal in diagonals:
+                    return False
+                return True
+           
+            # 2. Iterate through each col of the current row
+            localSolutions = 0
             for col in range(n):
-                curr_diagonal = row - col
-                curr_anti_diagonal = row + col
-                # If the queen is not placeable
-                if (col in cols 
-                      or curr_diagonal in diagonals 
-                      or curr_anti_diagonal in anti_diagonals):
+                localAntiDiagonal = row+col
+                localDiagonal = row-col
+                if not safePosition():
                     continue
+                colSet.add(col)
+                antiDiagonals.add(localAntiDiagonal)
+                diagonals.add(localDiagonal)
 
-                # "Add" the queen to the board
-                cols.add(col)
-                diagonals.add(curr_diagonal)
-                anti_diagonals.add(curr_anti_diagonal)
-
-                # Move on to the next row with the updated board state
-                solutions += backtrack(row + 1, diagonals, anti_diagonals, cols)
-
-                # "Remove" the queen from the board since we have already
-                # explored all valid paths using the above function call
-                cols.remove(col)
-                diagonals.remove(curr_diagonal)
-                anti_diagonals.remove(curr_anti_diagonal)
-
-            return solutions
-
+                localSolutions += backtrack(row+1, diagonals, antiDiagonals, colSet)
+                
+                colSet.remove(col)
+                diagonals.remove(localDiagonal)
+                antiDiagonals.remove(localAntiDiagonal)
+                
+            
+            return localSolutions
         return backtrack(0, set(), set(), set())
