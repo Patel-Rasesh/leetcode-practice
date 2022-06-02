d00047ca7326183872986df5988ea2db1aead2d9
Rasesh Patel <91232193+Patel-Rasesh@users.noreply.github.com>
2022-06-01 22:03:46 -0500

    Time: 24 ms (47.05%), Space: 13.5 MB (76.96%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -4,16 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        lengthOfLastWord = 0
-        # 1. Start iterating in a loop in reverse
-        for charac in range(len(s)-1, -1, -1):
-            # 2. Continue loop until a character is encountered
+# Solution 1
+#         lengthOfLastWord = 0
+#         # 1. Start iterating in a loop in reverse
+#         for charac in range(len(s)-1, -1, -1):
+#             # 2. Continue loop until a character is encountered
             
-            if s[charac] == ' ':
-                if lengthOfLastWord != 0:
-                    return lengthOfLastWord
-                continue
-            # 3. Count until next space is encountered
-            lengthOfLastWord += 1
+#             if s[charac] == ' ':
+#                 if lengthOfLastWord != 0:
+#                     return lengthOfLastWord
+#                 continue
+#             # 3. Count until next space is encountered
+#             lengthOfLastWord += 1
         
-        return lengthOfLastWord
+#         return lengthOfLastWord
+
+#           Solution 2
+        return len(s.split().pop(-1))
