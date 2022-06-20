00030913405c2f9ba443facea59a196cee0cb8f6
Rasesh Patel <91232193+Patel-Rasesh@users.noreply.github.com>
2022-06-20 11:01:27 -0500

    Time: 4232 ms (6.62%), Space: 14.3 MB (82.10%) - LeetHub

---
+++ 752-open-the-lock/752-open-the-lock.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def openLock(self, deadends, target):
+        """
+        :type deadends: List[str]
+        :type target: str
+        :rtype: int
+        """
+        queue = collections.deque([('0000', 0)])
+        explored = set()
+        explored.add('0000')
+        turns = 1
+        
+        def Neighbors(position):
+            neighbors = []
+            
+            # For each place of each digit
+            for eachDigit in range(4):
+                digit = position[eachDigit]
+                for diff in (-1, 1):
+                    
+                    # -1 mod 10 = 9
+                    newPosition = position[:eachDigit] + str((int(digit)+diff)%10) + position[eachDigit+1:]
+                    neighbors.append(newPosition)
+            return neighbors
+        
+        if '0000' == target:
+            return 0
+        
+        while queue:
+            currentComb, turns = queue.popleft()
+            if currentComb == target:
+                print(currentComb)
+                return turns
+            if currentComb in deadends:
+                continue
+            for eachComb in Neighbors(currentComb):
+                if eachComb not in explored:
+                    queue.append((eachComb, turns+1))
+                    explored.add(eachComb)
+        return -1
+        # print(Neighbors('3926'))
