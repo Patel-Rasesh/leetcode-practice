ad58e71037cc3c9f167a044418c90c920bdbf911
Rasesh Patel <91232193+Patel-Rasesh@users.noreply.github.com>
2022-08-25 18:28:42 -0500

    Time: 655 ms (99.21%), Space: 14.4 MB (83.46%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
@@ -4,14 +4,28 @@ class Solution(object):
         :type sticks: List[int]
         :rtype: int
         """
+#         heapq.heapify(sticks)
+#         totalCost = 0
+        
+#         while len(sticks) > 1:
+#             stick1 = heapq.heappop(sticks)
+#             stick2 = heapq.heappop(sticks)
+#             heapq.heappush(sticks, stick1+stick2)
+#             totalCost += stick1+stick2
+        
+#         return totalCost
+        
+    
+        # AMAZON INTERVIEW PREP
+        
+        if len(sticks) == 1:
+            return 0
         heapq.heapify(sticks)
         totalCost = 0
         
         while len(sticks) > 1:
-            stick1 = heapq.heappop(sticks)
-            stick2 = heapq.heappop(sticks)
-            heapq.heappush(sticks, stick1+stick2)
-            totalCost += stick1+stick2
+            localCost = heapq.heappop(sticks) + heapq.heappop(sticks)
+            heapq.heappush(sticks, localCost)
+            totalCost += localCost 
         
         return totalCost
-        
