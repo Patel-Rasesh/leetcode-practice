2c99e29881bc0cb6fed799648bf19cc179144d72
Rasesh Patel <91232193+Patel-Rasesh@users.noreply.github.com>
2022-09-26 18:04:24 -0500

    Time: 659 ms (40.23%), Space: 21.6 MB (77.87%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        m = len(text1)-1
+        n = len(text2)-1
+        
+#         Recursive solution - TLE
+#         def LCSrecur(i, j):
+#             if i > m or j > n:
+#                 return 0
+#             ignore = max(LCSrecur(i+1, j), LCSrecur(i, j+1))
+#             best = ignore
+#             if text1[i] == text2[j]:
+#                 include = 1 + LCSrecur(i+1, j+1)
+#                 if include > best:
+#                     best = include
+#             return best
+        
+#         return LCSrecur(0, 0)
+        
+        # DP solution
+        def LCSDP():
+            # 1. Frame a 2D array, (x,y) both decreasing, and initialize with base cases
+            table = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+            
+            # 2. Two for loops to iterate through the table (2D array) and fill the table entries up
+            for i in reversed(range(m+1)):
+                for j in reversed(range(n+1)):
+               
+                    # 3. Rest of the code is same, only replace table entries in place of recursive calls
+                    ignore = max(table[i+1][j], table[i][j+1])
+                    best = ignore
+                    if text1[i] == text2[j]:
+                        include = 1 + table[i+1][j+1]
+                        if include > best:
+                            best = include
+                    table[i][j] = best
+                    
+            return table[0][0]    
+        return LCSDP()
