a883ded6fd871b89e3a57020100e345d4573cc13
Rasesh Patel <91232193+Patel-Rasesh@users.noreply.github.com>
2022-10-14 10:58:14 -0500

    Time: 34 ms (38.89%), Space: 13.3 MB (67.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,23 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Recursive solution
         n = len(nums)
-        memo = [None]*n
+        # memo = [None]*n
         
-        # Recursive solution
-        def robRecur(index):
-            if index >= n:
-                return 0
-            if memo[index] is None:
-                ignore = robRecur(index+1)
-                best = ignore
-                include = robRecur(index+2)+nums[index]
-                if include > best:
-                    best = include
-                memo[index] = best
-            return memo[index]
+        # Recursive solution with memoization
+#         def robRecur(index):
+#             if index >= n:
+#                 return 0
+#             if memo[index] is None:
+#                 ignore = robRecur(index+1)
+#                 best = ignore
+#                 include = robRecur(index+2)+nums[index]
+#                 if include > best:
+#                     best = include
+#                 memo[index] = best
+#             return memo[index]
         
-        return robRecur(0)
+#         return robRecur(0)
         
         # DP solution
+        DPTable = [None]*(n+1)
+        print(DPTable)
+        DPTable[n] = 0
+        DPTable[n-1] = nums[n-1]
+        print(DPTable)
+        for index in range(n-2, -1, -1):
+            DPTable[index] = max(DPTable[index+1], nums[index]+DPTable[index+2])
+        print(DPTable)
+        return DPTable[0]
